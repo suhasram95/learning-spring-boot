cbfbe7e14f5a3e9610bcb8791697dfe9ea4d65cc
Suhas Ramesh <suhasram95@gmail.com>
2022-03-28 06:45:27 -0400

    Initial Commit

---
+++ src/test/java/com/sample/learningspringboot/LearningSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.learningspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
